aeabd3ed18e4aaeb81194c8e922556b04b764250
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-03-03 18:02:32 +0100

    feat: streak days

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -38,6 +38,33 @@ const LandingPage = () => {
     loadTopStepData();
   }, [currentUser]);
 
+  /* count last streak day by day, if there is a gap - stop counting */
+  function countStreakDays(loginDates: string[]): number {
+    if (loginDates.length === 0) {
+      return 0;
+    }
+
+    const sortedDates = loginDates.sort((a, b) => new Date(b).getTime() - new Date(a).getTime());
+    let count = 1;
+    let lastDate = new Date(sortedDates[0]);
+
+    for (let i = 1; i < sortedDates.length; i++) {
+      const currentDate = new Date(sortedDates[i]);
+      const lastDateTime = lastDate.getTime();
+      const currentDateTime = currentDate.getTime();
+      const diffDays = Math.floor((lastDateTime - currentDateTime) / (1000 * 60 * 60 * 24));
+
+      if (diffDays === 1) {
+        count++;
+        lastDate = currentDate;
+      } else {
+        break;
+      }
+    }
+
+    return count;
+  }
+
   return (
     <div>
       <h1>Lider kroków</h1>
@@ -49,10 +76,11 @@ const LandingPage = () => {
               const currentMonth = `${new Date().getMonth() + 1}`.padStart(2, "0");
               const currentYear = new Date().getFullYear();
               const currentDate = `${currentYear}-${currentMonth}`;
+              const streakDays = countStreakDays(top.loginDates);
 
               return (
                 <li key={top.id}>
-                  {index + 1}. {top.displayName} - {top.stepsData[currentDate]}
+                  {index + 1}. {top.displayName} - {top.stepsData[currentDate]} - {streakDays}
                 </li>
               );
             })}
